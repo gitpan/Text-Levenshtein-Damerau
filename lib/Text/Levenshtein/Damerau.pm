94081589d95515da184be2141c39d0404b139fc1
Nick Logan <ugexe@cpan.org>
2012-06-21 02:38:45 +0000

    Import of UGEXE/Text-Levenshtein-Damerau-0.04 from CPAN.
    gitpan-cpan-distribution: Text-Levenshtein-Damerau
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         UGEXE/Text-Levenshtein-Damerau-0.04.zip
    gitpan-cpan-author:       UGEXE
    gitpan-cpan-maturity:     released

--- lib/Text/Levenshtein/Damerau.pm
+++ lib/Text/Levenshtein/Damerau.pm
@@ -5,7 +5,7 @@ use warnings;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 use List::Util qw(min);
 
-$VERSION     = '0.03';
+$VERSION     = '0.04';
 @ISA         = qw(Exporter);
 @EXPORT      = ();
 @EXPORT_OK   = qw(&edistance);
