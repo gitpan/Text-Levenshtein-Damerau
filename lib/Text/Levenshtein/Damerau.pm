fbc01cf44b5265f5a7a7bf5a2f0d013044176e73
Nick Logan <ugexe@cpan.org>
2012-10-26 21:54:56 +0000

    Import of UGEXE/Text-Levenshtein-Damerau-0.20 from CPAN.
    gitpan-cpan-distribution: Text-Levenshtein-Damerau
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         UGEXE/Text-Levenshtein-Damerau-0.20.zip
    gitpan-cpan-author:       UGEXE
    gitpan-cpan-maturity:     released

--- lib/Text/Levenshtein/Damerau.pm
+++ lib/Text/Levenshtein/Damerau.pm
@@ -1,19 +1,26 @@
-##############################################################################
-#      $URL: https://github.com/ugexe/Text--Levenshtein--Damerau $
-#     $Date: 2012-10-25 00:02:39 -0500 (Thu, 25 Oct 2012) $
-#   $Author: ugexe $
-# $Revision: 4200 $
-##############################################################################
-
 package Text::Levenshtein::Damerau;
+use Text::Levenshtein::Damerau::PP;
+use strict;
 use utf8;
 use List::Util qw/reduce min/;
-use strict;
-use warnings;
-use Exporter qw( import );
+use Exporter qw/import/;
 
-our @EXPORT_OK = qw( edistance );
-our $VERSION   = '0.19';
+our @EXPORT_OK = qw/edistance/;
+our $VERSION = '0.20';
+
+
+# To XS or not to XS...
+eval {
+	require Text::Levenshtein::Damerau::XS;
+};
+if(!$@) {
+  *edistance = \&Text::Levenshtein::Damerau::XS::xs_edistance;
+}
+else {
+  *edistance = \&Text::Levenshtein::Damerau::PP::pp_edistance;
+}
+
+
 
 sub new {
     my $class = shift;
@@ -79,93 +86,13 @@ sub dld_best_distance {
     }
 }
 
-sub edistance {
-
-    # Does the actual calculation on a pair of strings
-    my ( $source, $target ) = @_;
-    if ( _null_or_empty($source) ) {
-        if ( _null_or_empty($target) ) {
-            return 0;
-        }
-        else {
-            return length($target);
-        }
-    }
-    elsif ( _null_or_empty($target) ) {
-        return length($source);
-    }
-    elsif ( $source eq $target ) {
-        return 0;
-    }
-
-    my $m   = length($source);
-    my $n   = length($target);
-    my $INF = $m + $n;
-    my %H;
-    $H{0}{0} = $INF;
-
-    for ( 0 ... $m ) {
-        my $i = $_;
-        $H{ $i + 1 }{1} = $i;
-        $H{ $i + 1 }{0} = $INF;
-    }
-    for ( 0 .. $n ) {
-        my $j = $_;
-        $H{1}{ $j + 1 } = $j;
-        $H{0}{ $j + 1 } = $INF;
-    }
-
-    my %sd;
-    for ( 0 .. ( $m + $n ) ) {
-        my $letter = substr( $source . $target, $_ - 1, 1 );
-        $sd{$letter} = 0;
-    }
-
-    for ( 1 .. $m ) {
-        my $i  = $_;
-        my $DB = 0;
-
-        for ( 1 .. $n ) {
-            my $j  = $_;
-            my $i1 = $sd{ substr( $target, $j - 1, 1 ) };
-            my $j1 = $DB;
-
-            if ( substr( $source, $i - 1, 1 ) eq substr( $target, $j - 1, 1 ) )
-            {
-                $H{ $i + 1 }{ $j + 1 } = $H{$i}{$j};
-                $DB = $j;
-            }
-            else {
-                $H{ $i + 1 }{ $j + 1 } =
-                  min( $H{$i}{$j}, $H{ $i + 1 }{$j}, $H{$i}{ $j + 1 } ) + 1;
-            }
-
-            $H{ $i + 1 }{ $j + 1 } = min( $H{ $i + 1 }{ $j + 1 },
-                $H{$i1}{$j1} + ( $i - $i1 - 1 ) + 1 + ( $j - $j1 - 1 ) );
-        }
-
-        $sd{ substr( $source, $i - 1, 1 ) } = $i;
-    }
-
-    return $H{ $m + 1 }{ $n + 1 };
-}
-
-sub _null_or_empty {
-    my $s = shift;
-
-    if ( defined($s) && $s ne {} ) {
-        return 0;
-    }
-
-    return 1;
-}
-
 1;
+
 __END__
 
 =head1 NAME
 
-C<Text::Levenshtein::Damerau> - Damerau Levenshtein edit distance
+C<Text::Levenshtein::Damerau> - Damerau Levenshtein edit distance.
 
 =head1 SYNOPSIS
 
@@ -191,9 +118,18 @@ C<Text::Levenshtein::Damerau> - Damerau Levenshtein edit distance
 	print $tld->dld_best_distance({ list => \@targets });
  	# prints 1
 
+
+	# or even more simply
+	use Text::Levenshtein::Damerau qw/edistance/;
+	use warnings;
+	use strict;
+	
+	print edistance('Neil','Niel');
+	# prints 1
+
 =head1 DESCRIPTION
 
-Returns the true Damerau Levenshtein edit distance of strings with adjacent transpositions.
+Returns the true Damerau Levenshtein edit distance of strings with adjacent transpositions. Defaults to using Pure Perl L<Text::Levenshtein::Damerau::PP>, but has an XS addon L<Text::Levenshtein::Damerau::XS> for massive speed imrovements.
 
 =head1 CONSTRUCTOR
 
@@ -267,7 +203,15 @@ Arguments: source string and target string.
 
 Returns: scalar containing int that represents the edit distance between the two argument.
 
-Function to take the edit distance between a source and target string. Contains the actual algorithm implementation 
+Wrapper function to take the edit distance between a source and target string. It will attempt to use, in order: 
+
+=over 4
+
+=item * L<Text::Levenshtein::Damerau::XS> B<xs_edistance>
+
+=item * L<Text::Levenshtein::Damerau::PP> B<pp_edistance>
+
+=back
 
 	use Text::Levenshtein::Damerau qw/edistance/;
 	print edistance('Neil','Niel');
@@ -298,3 +242,5 @@ ugexe <F<ug@skunkds.com>>
 This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =cut
+
+
